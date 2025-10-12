707b2fae646a220557d5cfc1fb79f2ab169348e9
Rohith-Dasari <rohithdasariformal@gmail.com>
2025-10-12 22:55:14 +0530

    fixed color problem

--- src/app/shared/spinner/spinner.component.ts
+++ src/app/shared/spinner/spinner.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 
 @Component({
   selector: 'app-spinner',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './spinner.component.html',
   styleUrl: './spinner.component.scss'
 })
